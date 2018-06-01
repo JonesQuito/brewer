3e39d62615d5fd7e825bb13ca0783db7809043bf
JonesQuito <jonesdhy@hotmail.com>
2018-06-01 09:27:48 -0300

    Configuração do JPA e implementação dos combos da tela de cadastro de cervejas

--- src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -11,7 +11,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[] {JPAConfig.class};
 	}
 
 	@Override
